520e17186b64c4ed1da3b74f4ef028ffd8d41e98
Ismail Mayat <imayat@thecogworks.co.uk>
2017-05-26 10:56:14 +0100

    inital commit

---
+++ src/Cogworks.ExamineFileIndexer/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace Cogworks.ExamineFileIndexer
+{
+    public class Constants
+    {
+        public const string PackageName = "ExamineFileIndexer";
+
+        public const string PackageFilesPath = "~/App_Plugins/" + PackageName + "/";
+
+        
+    }
+}
